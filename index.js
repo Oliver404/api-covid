2fdd4d851aae13938f1bf872c1a87d9ca6a4ca55
Oliver Botello <oliverbotello95@gmail.com>
2022-08-05 00:46:33 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const axios = require("axios")
+const app = express();
+
+function onSuccessCountries(response) {
+    this.send(response.data);
+    console.log(response);
+}
+function onErrorCountries(error) {
+    this.send("error");
+    console.log(error);
+}
+
+function onFinallyCountries() {
+    console.log("Termino");
+}
+
+app.get('/', function (req, res) {
+    // res.send('Saludos desde express');
+    axios.get("https://restcountries.com/v3.1/all")
+        .then(onSuccessCountries.bind(res))
+        .catch(onErrorCountries.bind(res))
+        .finally(onFinallyCountries.bind(res));
+});
+
+app.listen(3000, () => {
+    console.log("El servidor está inicializado en el puerto 3000");
+});
+
+// https://developer.mozilla.org/en-US/docs/Learn/Server-side/Express_Nodejs/deployment#example_installing_locallibrary_on_heroku
